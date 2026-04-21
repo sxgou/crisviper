fe40ecbaaa9938aeacc5a8b166951c25a8582a00
gsx-ucas <13610294176@163.com>
2026-04-21 10:59:13 +0800

    add test.sh

---
+++ test.sh
@@ -0,0 +1,9 @@
+lineage-tracer analyze \
+--fastq_r1 test_data/EPSC2_L1_1.fq.gz \
+--fastq_r2 test_data/EPSC2_L1_2.fq.gz \
+--config test_data/EPSC2_config.json \
+--output_dir ./results \
+--sample_name EPSC2 \
+--kmer_size 6 \
+--min_overlap 10 \
+--threads 10
